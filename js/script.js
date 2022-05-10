b75f7069d7232ca5a497aaecb9a817723164ba97
sayabi1 <95686914+sayabi1@users.noreply.github.com>
2022-05-10 07:12:27 +0200

    pokemonList variable
    list of pokemons created

---
+++ js/script.js
@@ -0,0 +1,3 @@
+let pokemonList =[{name:'Bulbasaur', height:7, type:['poison','grass']},
+{name:'Ivysaur', height:1, type:['poison','grass'] },   
+{name:'Venusaur', height:2, type:['monster','grass'] }];
