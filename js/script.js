2ab44201e66f8cc3e0119333ab894c9050be55b4
sayabi1 <95686914+sayabi1@users.noreply.github.com>
2022-05-10 07:23:31 +0200

    pokemons added with extra features

--- js/script.js
+++ js/script.js
@@ -1,3 +1,47 @@
-let pokemonList =[{name:'Bulbasaur', height:7, type:['poison','grass']},
-{name:'Ivysaur', height:1, type:['poison','grass'] },   
-{name:'Venusaur', height:2, type:['monster','grass'] }];
+let pokemonList = [{name:'Bulbasaur',
+height:7,
+weight:'6.9 kg',
+Abilities:['Chlorophyll','overgrow'],
+EggGroups:['Monster','Grass'],
+type:['poison','grass']
+},
+
+{name:'Ivysaur',
+ height:10,
+ Weight:'13 kg',
+ Abilities:['Chlorophyll','overgrow'],
+ EggGroups:['Monster','Grass'],
+ type:['poison','grass']
+},   
+
+{name:'Venusaur',
+ height:20,
+ Weight:'100 kg',
+ Abilities:['Chlorophyll','overgrow'],
+ EggGroups:['Monster','Grass'],
+ type:['monster','grass']
+}, 
+
+{name:'Charmender',
+ height:6,
+ Weight:'8.5 kg',
+ Abilities:['blaze','solar-power'],
+ EggGroups:['Monster','Dragon'],
+ type:['fire']
+}, 
+
+{name:'charmeleon',
+ height:11,
+ Weight:'19 kg',
+ Abilities:['blaze','solar-power'],
+ EggGroups:['Monster','Dragon'],
+ type:['fire']
+}, 
+
+{name:'charizard',
+ height:17,
+ Weight:'90.5 kg',
+ Abilities:['blaze','solar-power'],
+ EggGroups:['Monster','Dragon'],
+ type:['fire','flying']
+}]; 
