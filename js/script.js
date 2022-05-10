da92a30413af2ad5134cebf538023a5ec60dfa20
sayabi1 <95686914+sayabi1@users.noreply.github.com>
2022-05-10 10:08:22 +0200

    added loadList and loadDetails function
    Added function(s) inside the data repository to load data from an external source.,
    forEach Pokémon, called the addListItem() function ,
    Inside the showDetails() function, called the loadDetails() function from above. Pass as parameter the Pokémon object.

--- js/script.js
+++ js/script.js
@@ -1,100 +1,83 @@
-let pokemonRepository = (function(){
-    let pokemonList = [
-   {
-     name: 'Bulbasaur',
-     height: 0.7,
-     weight: '6.9 kg',
-     Abilities: ['Chlorophyll', 'overgrow'],
-     EggGroups: ['Monster', 'Grass'],
-     type: ['poison', 'grass']
-   },
+let pokemonRepository = (function () {
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-   {
-     name: 'Ivysaur',
-     height: 1,
-     Weight: '13 kg',
-     Abilities: ['Chlorophyll', 'overgrow'],
-     EggGroups: ['Monster', 'Grass'],
-     type: ['poison', 'grass']
-   },
-
-   {
-     name: 'Venusaur',
-     height: 2,
-     Weight: '100 kg',
-     Abilities: ['Chlorophyll', 'overgrow'],
-     EggGroups: ['Monster', 'Grass'],
-     type: ['monster', 'grass']
-   },
-
-   {
-     name: 'Charmender',
-     height: 0.6,
-     Weight: '8.5 kg',
-     Abilities: ['blaze', 'solar-power'],
-     EggGroups: ['Monster', 'Dragon'],
-     type: ['fire']
-   },
-
-   {
-     name: 'charmeleon',
-     height: 1.1,
-     Weight: '19 kg',
-     Abilities: ['blaze', 'solar-power'],
-     EggGroups: ['Monster', 'Dragon'],
-     type: ['fire']
-   },
-
-   {
-     name: 'charizard',
-     height: 1.7,
-     Weight: '90.5 kg',
-     Abilities: ['blaze', 'solar-power'],
-     EggGroups: ['Monster', 'Dragon'],
-     type: ['fire', 'flying']
-   }
- ];
-
- function add (pokemon) {
-   pokemonList.push(pokemon)
- }
-
- function getAll () {
-   return pokemonList
- }
-  function showDetails(pokemon) {
-      console.log(pokemon);
+  function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon
+    ) {
+      pokemonList.push(pokemon);
+    } else {
+      console.log("pokemon is not correct");
+    }
   }
- function addListItem(pokemon){
-    let pokemons = document.querySelector('.pokemon-list');
-    let listItem = document.createElement('li');
-    listItem.classList.add("pokemon-list__item")
-    let button = document.createElement('button');
+  function getAll() {
+    return pokemonList;
+  }
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listpokemon = document.createElement("li");
+    listpokemon.classList.add("pokemon-list__item")
+    let button = document.createElement("button");
     button.innerText = pokemon.name;
-    button.classList.add('btn');
-    listItem.appendChild(button);
-    pokemons.appendChild(listItem);
+    button.classList.add("btn");
+    listpokemon.appendChild(button);
+    pokemonList.appendChild(listpokemon);
     button.addEventListener("click", function(event) {
-        showDetails(pokemon);
+      showDetails(pokemon);
     });
- }
- return {
-   add: add,
-   getAll: getAll,
-   addListItem:addListItem,
-   showDetails:showDetails
- }
+  }
 
- })();
- console.log(pokemonRepository.getAll())
- pokemonRepository.add({ name: 'squirtel', height: 0.5,  weight:'9 kg', Abilities:['rain-dish', 'torrent'], type: 'water' })
- console.log(pokemonRepository.getAll())
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+        console.log(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
 
+  function loadDetails(pokemon) {
+    let url = pokemon.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      // Now we add the details to the pokemon
+      pokemon.imageUrl = details.sprites.front_default;
+      pokemon.height = details.height;
+      pokemon.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
 
-let bigPokemon = 'Wow, that is big!';
-let smallPokemon ="that is small!"
+  function showDetails(item) {
+    pokemonRepository.loadDetails(item).then(function () {
+      console.log(item);
+    });
+  }
 
-// forEach loop and conditional statement
-pokemonRepository.getAll().forEach(function (pokemon) {
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails
+  };
+})();
+pokemonRepository.loadList().then(function () {
+  pokemonRepository.getAll().forEach(function (pokemon) {
     pokemonRepository.addListItem(pokemon);
-})
+  });
+});
+
